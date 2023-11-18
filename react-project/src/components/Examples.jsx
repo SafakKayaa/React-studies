8d261845a7f7c78fdd2d4bd65f41edb51305af01
Şafak Kaya <108135774+SafakKayaa@users.noreply.github.com>
2023-11-18 16:36:06 +0100

    section part is splitted

--- react-project/src/components/Examples.jsx
+++ react-project/src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import TabButton from "./TabButton.jsx";
 import { EXAMPLES } from "../data.js";
+import Section from "./Section.jsx";
 
 export default function Examples() {
     const [selectedTopic, setSelectedTopic] = useState("");
@@ -14,30 +15,29 @@ export default function Examples() {
     }
 
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title="Examples" id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
-          onSelect={() => handleSelect("components")}
+          onClick={() => handleSelect("components")}
         >
           Components
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "jsx"}
-          onSelect={() => handleSelect("jsx")}
+          onClick={() => handleSelect("jsx")}
         >
           Jsx
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "props"}
-          onSelect={() => handleSelect("props")}
+          onClick={() => handleSelect("props")}
         >
           Props
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "state"}
-          onSelect={() => handleSelect("state")}
+          onClick={() => handleSelect("state")}
         >
           State
         </TabButton>
@@ -52,6 +52,6 @@ export default function Examples() {
           </pre>
         </div>
       )}
-    </section>
+    </Section>
   );
 }
